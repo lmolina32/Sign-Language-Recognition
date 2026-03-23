67690f8586a3bcbc219f22376f6aaa001126436e
Leonardo Molina <lmolina3@nd.edu>
2026-03-22 20:02:15 -0500

    feat: create src + outline for pipeline

---
+++ src/pipeline.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import cv 
+import numpy as np 
+import os 
+
+class Preprocessor:
+    def __init__(self):
+        pass 
+    
+class Segmentation:
+    def __init__(self):
+        pass
+    
